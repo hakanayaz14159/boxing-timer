f6bb7f91396db68838ee8d03ed436df1bc327275
Hakan Ayaz <hakanayaz14159@outlook.com>
2026-03-10 04:26:34 +0300

    Add GitHub repo URL to icon link and package.json

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,1 @@
-/**
- * Set this to your GitHub repo URL when publishing (e.g. https://github.com/username/box-timer)
- */
-export const GITHUB_REPO_URL = 'https://github.com';
+export const GITHUB_REPO_URL = 'https://github.com/hakanayaz14159/boxing-timer';
